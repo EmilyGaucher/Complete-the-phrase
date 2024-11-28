0964bda1d372709bd496175127757f50dcc265db
EmilyGaucher <e.gaucher@share.epsb.ca>
2024-11-28 14:18:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Complete The Phrase
+
+document.getElementById("around").addEventListener("click", answer);
+
+function answer() {
+  var idiom1 = "the bush";
+
+  alert(idiom1);
+}
+
+document.getElementById("leg").addEventListener("click", Answer);
+
+function Answer() {
+  var idiom2 = "leg";
+
+  alert(idiom2);
+}
+
+document.getElementById("sack").addEventListener("click", sack);
+
+function sack() {
+  var idiom3 = "sack";
+
+  alert(idiom3);
+}
+
+document.getElementById("science").addEventListener("click", science);
+
+function science() {
+  var idiom4 = "science";
+
+  alert(idiom4);
+}
+
+document.getElementById("devil").addEventListener("click", devil);
+
+function devil() {
+  var idiom5 = "devil";
+
+  alert(idiom5);
+}
